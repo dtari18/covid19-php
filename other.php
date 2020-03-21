0a6a39a5b3551777936b1583dfa4dc729bcde6b6
Dwi A Lestari <dwitari1809@gmail.com>
2020-03-21 12:10:58 +0700

    Update other.php

--- other.php
+++ other.php
@@ -10,7 +10,6 @@ else{
 	$negara = "Indonesia";
 }
 $curl = curl_init();
-
 curl_setopt_array($curl, array(
 	CURLOPT_URL => "https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php?country=". $id,
 	CURLOPT_RETURNTRANSFER => true,
@@ -26,16 +25,12 @@ curl_setopt_array($curl, array(
 		"x-rapidapi-key: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 	),
 ));
-
 $response = curl_exec($curl);
 $err = curl_error($curl);
-
 curl_close($curl);
-
 if ($err) {
 	$teks = "<p align='center' class='label label-danger'>Ada kesalahan atau error pada server.</p>";
 } else {
-	
 	$json_objek = json_decode($response, true);
 	$teks = "";
 }
@@ -47,23 +42,17 @@ if ($err) {
 		<title>Aplikasi Pemantauan COVID-19 Negara Lain</title>
 		<!-- Description, Keywords and Author -->
 		<meta name="description" content="Status COVID-19.">
-		<meta name="keywords" content="DWITARI">
-		
-		
+		<meta name="keywords" content="COVID-19">
+		<meta name="author" content="Dwi A. Lestari">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		
 		<!-- Styles -->
 		<!-- Bootstrap CSS -->
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 		<!-- Font awesome CSS -->
-		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">		
-		<!-- Custom CSS -->
-	
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 		<link href="//fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600" rel="stylesheet">
 	</head>
-	
 	<body>
-	
 		<div class="wrapper">
 			<!-- header -->
 			<header>
@@ -77,24 +66,18 @@ if ($err) {
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
-							</button>
-							
+							</button>					
 						</div>
-
 						<!-- Collect the nav links, forms, and other content for toggling -->
 						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 							<ul class="nav navbar-nav">
 								<li class="active"><a href="index.php">Beranda</a></li>
 								<li class="active"><a href="other.php">Negara Lain</a></li>
 							</ul>
-							
 						</div><!-- /.navbar-collapse -->
 					</div><!-- /.container-fluid -->
 				</nav>
 			</header>
-			
-			
-				
 				<!-- benefits -->
 				<div class="benefits pad">
 					<h2 align="center">PEMANTAUAN COVID-19 <?php echo $negara; ?> </h2>
@@ -135,25 +118,15 @@ if ($err) {
 						</div>
 						</div>
 					</div>
-				
-				
-				
-				
-				
-				
-				
-			
 			<!-- footer -->
 			<footer>
 				<div class="container"">
 					<div class="row" style="background:#dddddd;">
-					
 					<p class="copy-right">Copyright &copy; 2020 <a href="https://dwitari.my.id/covid19/stat">www.dwitari.my.id</a>, All rights reserved. </p>
 				</div>
 				</div>
 			</footer>
 		</div>		
-		
 		<!-- Javascript files -->
 		<!-- jQuery -->
 		<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
